642cd3d60f9bf303ff719e811ac2260947ed1cc3
Stephen Perrin <stephenperrin@Stephens-MacBook-Pro.local>
2023-09-14 14:27:57 +0200

    robots.txt added + meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ export default function PorfolioStephen({ Component, pageProps }: AppProps) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
         />
+        <meta name="description" content="Le portfolio de Stephen Perrin" />
       </Head>
       <Component {...pageProps} />
     </>
